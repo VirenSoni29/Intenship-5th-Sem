237b53de0953d58bfc638bcde6e06639f3bca5bc
Viren Soni <71062471+VirenSoni-Dev@users.noreply.github.com>
2026-06-23 10:46:54 +0530

    Fixed server not working

--- backend/config/config.js
+++ backend/config/config.js
@@ -2,22 +2,24 @@ import dotenv from 'dotenv';
 
 dotenv.config();
 
-export default config = {
-   PORT=process.env.PORT,
+const config = {
+   PORT: process.env.PORT,
 
-   DB_HOST=process.env.DB_HOST,
-   DB_USER=process.env.DB_USER,
-   DB_PASSWORD=process.env.DB_PASSWORD,
-   DB_PORT=process.env.DB_PORT,
-   DB_NAME=process.env.DB_NAME,
+   DB_HOST: process.env.DB_HOST,
+   DB_USER: process.env.DB_USER,
+   DB_PASSWORD: process.env.DB_PASSWORD,
+   DB_PORT: process.env.DB_PORT,
+   DB_NAME: process.env.DB_NAME,
 
-   JWT_SECRET=process.env.JWT_SECRET,
-   JWT_REFRESH_SECRET=process.env.JWT_REFRESH_SECRET,
+   JWT_SECRET: process.env.JWT_SECRET,
+   JWT_REFRESH_SECRET: process.env.JWT_REFRESH_SECRET,
 
-   EMAIL_USER=process.env.EMAIL_USER,
-   EMAIL_PASS=process.env.EMAIL_PASS,
+   EMAIL_USER: process.env.EMAIL_USER,
+   EMAIL_PASS: process.env.EMAIL_PASS,
 
-   NODE_ENV=process.env.NODE_ENV,
+   NODE_ENV: process.env.NODE_ENV,
 
-   FRONTEND_URL=process.env.FRONTEND_URL
+   FRONTEND_URL: process.env.FRONTEND_URL
 };
+
+export default config
